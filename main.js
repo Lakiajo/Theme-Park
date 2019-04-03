7cc1afdc38d3e92d6beeb99025f110c4ee3e39c7
Lakia Jones <rachandra_jones@yahoo.com>
2019-04-02 21:30:09 -0500

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-Console.log('Lakia Jones')
+console.log('Lakia Jones');
+// XHR-xmlHttpRequest
+
+let rides = [];
+
+const domStringBuilder = (arrayToPrint) => {
+    console.log(arrayToPrint);
+};
+
+function executeThisCodeAfterFileLoads(){
+    const data = JSON.parse(this.responseText);
+    // console.log('Yay!', data);
+    rides = data.rides;
+    domStringBuilder(data.rides);
+};
+
+function executeThisCodeIfXHRFails(){
+    console.error('OhShit');
+};
+
+const getRidesData = () => {
+    const myRequest = new XMLHttpRequest ();
+    myRequest.addEventListener('load', executeThisCodeAfterFileLoads);
+    myRequest.addEventListener('error', executeThisCodeIfXHRFails);
+    myRequest.open('GET', './db/rides.json');
+    myRequest.send();
+    console.log(myRequest);
+};
+
+
+const init = () => {
+    getRidesData(); 
+};
+
+init();
